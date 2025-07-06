3f49725b20466eeafea37c7aa9c0502840b568d3
shokoko2010 <41647560+shokoko2010@users.noreply.github.com>
2025-07-06 17:59:36 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cwd } from 'node:process'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  }
 })
