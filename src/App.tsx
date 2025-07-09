6064c7afba9e58b69aeeb5cd0bced94ba5a46ed9
shokoko2010 <41647560+shokoko2010@users.noreply.github.com>
2025-07-09 17:11:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState, useCallback, useEffect } from 'react';
 import PageSelectorPage from './components/PageSelectorPage';
 import DashboardPage from './components/DashboardPage';
